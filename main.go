d001b287267ecb67461dacb74ee7c7cad4277c14
CrazyMax <crazy-max@users.noreply.github.com>
2020-12-03 13:01:32 +0100

    Portapps 3.0.0

--- main.go
+++ main.go
@@ -9,11 +9,11 @@ import (
 	"os"
 	"path"
 
-	"github.com/portapps/portapps/v2"
-	"github.com/portapps/portapps/v2/pkg/log"
-	"github.com/portapps/portapps/v2/pkg/proc"
-	"github.com/portapps/portapps/v2/pkg/shortcut"
-	"github.com/portapps/portapps/v2/pkg/utl"
+	"github.com/portapps/portapps/v3"
+	"github.com/portapps/portapps/v3/pkg/log"
+	"github.com/portapps/portapps/v3/pkg/proc"
+	"github.com/portapps/portapps/v3/pkg/shortcut"
+	"github.com/portapps/portapps/v3/pkg/utl"
 	"github.com/portapps/skype-portable/assets"
 )
 
